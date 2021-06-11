a0d65a96acbe0d69c561347c65044eaacd4901be
Alexandre Yano <alexandreyano@hotmail.com>
2021-06-11 18:14:38 -0400

    App Bar and Gradient Background

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Cryptos by Price'),
+      ),
+      body: Container(
+        decoration: BoxDecoration(
+            gradient: LinearGradient(
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+          colors: [
+            Theme.of(context).primaryColor,
+            Colors.grey[900]!,
+          ],
+        )),
+      ),
+    );
+  }
+}
